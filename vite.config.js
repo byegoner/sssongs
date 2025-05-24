cd219382b626b16d7f034bd47b557be2026cc683
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2025-05-25 01:15:43 +0700

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./",
+  base: "/songs/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        entryFileNames: "songs.js",
+        chunkFileNames: "songs-[name].js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "songs.css";
+          }
+          return "assets/[name].[ext]";
+        },
+      },
+    },
   },
-  server: {
-    allowedHosts: true,
-  },
+  // server: {
+  //   allowedHosts: true,
+  // },
 });
